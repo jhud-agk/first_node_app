8e8ab2d72da0044d361ce1cd7c7f7e28c15e4e26
jude <judejike85@gmail.com>
2017-07-06 16:13:10 +0100

    logout for user

--- app.js
+++ app.js
@@ -84,6 +84,11 @@ require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('*', (req, res, next)=>{
+	res.locals.user = req.user || null;
+	next();
+});
+
 
 
 //Home route
