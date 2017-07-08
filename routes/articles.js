2d3e3e8637b96e9cf20d115a0a37c9db9c534a30
jude <judejike85@gmail.com>
2017-07-08 15:59:35 +0100

    css

--- routes/articles.js
+++ routes/articles.js
@@ -90,13 +90,21 @@ router.post('/edit/:id', (req,res)=>{
 
 //DELETE ARTICLE
 router.delete('/:id', (req,res)=>{
+	if(!req.user._id){
+		res.status(500).send();
+	}
 	let query = {_id:req.params.id }
-
-	Article.remove(query,(err)=>{
-		if(err){
-			console.log(err);
+	Article.findById(req.params.id, (err, article)=>{
+		if(article.author !=req.user._id){
+			res.status(500).send();
+		}else {
+			Article.remove(query,(err)=>{
+				if(err){
+					console.log(err);
+				}
+				res.send('success');
+			});	
 		}
-		res.send('success');
 	});
 });
 
